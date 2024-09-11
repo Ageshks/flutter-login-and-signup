b62f97b351c71277b97d3d41fb0e2e01e0316031
OCTOBERMAN <aneeshabalan99@gmail.com>
2024-09-11 12:14:42 +0530

    Initial commit

---
+++ Welcome-Login-Signup-Page-Flutter-master(2)/Welcome-Login-Signup-Page-Flutter-master/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Welcome/welcome_screen.dart';
+import 'package:flutter_auth/constants.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
+      theme: ThemeData(
+          primaryColor: const Color.fromARGB(255, 204, 28, 128),
+          scaffoldBackgroundColor: const Color.fromARGB(255, 0, 0, 0),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              foregroundColor: Colors.white,
+              backgroundColor: const Color.fromARGB(255, 238, 2, 2),
+              shape: const StadiumBorder(),
+              maximumSize: const Size(double.infinity, 56),
+              minimumSize: const Size(double.infinity, 56),
+            ),
+          ),
+          inputDecorationTheme: const InputDecorationTheme(
+            filled: true,
+            fillColor: Color.fromARGB(255, 247, 247, 247),
+            iconColor: Color.fromARGB(255, 8, 0, 0),
+            prefixIconColor: Color.fromARGB(255, 247, 247, 247),
+            contentPadding: EdgeInsets.symmetric(
+                horizontal: defaultPadding, vertical: defaultPadding),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+              borderSide: BorderSide.none,
+            ),
+          )),
+      home: const WelcomeScreen(),
+    );
+  }
+}
